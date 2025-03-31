17cb597237294e4709ce7e8ab448359becadd0fb
I am wuyanzu <12574809+lengpeihang--wuyanzu@user.noreply.gitee.com>
2025-03-31 10:39:47 +0800

    message 8:Before Refactoring the functional_ tests

--- functional_tests.py
+++ functional_tests.py
@@ -23,21 +23,14 @@ class NewVisitorTest(unittest.TestCase):
 
         # 应用有一个输入待办事项的文本输入框
         inputbox = self.browser.find_element(By.ID, 'id_new_item')  # (1)
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+        inputbox.send_keys('Give a gift to Lisi')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        # 他在文本输入框中输入了"Buy flowers"
-        inputbox.send_keys('Buy flowers')  # (2)
-        # 他按了回车键后，页面更新了
-        # 待办事项表格中显示了"1: Buy flowers"
-        inputbox.send_keys(Keys.ENTER)  # (3)
-        time.sleep(1)  # (4)
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')  # (1)
         self.assertIn('1: Buy flowers', [row.text for row in rows])
-
+        self.assertIn('2:Give a gift to Lisi', [row.text for row in rows])
         # 页面中又显示了一个文本输入框，可以输入其他待办事项
         # 他输入了"gift to girlfriend"
         self.fail('Finish the test!')
--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from django.urls import resolve
-from lists.views import home_page 
+from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+
 
 class HomePageTest(TestCase):
 
@@ -14,10 +15,15 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()#(1)
-        response = home_page(request)#(2)
-        html = response.content.decode('utf8')#(3)
-        self.assertTrue(html.startswith('<html>'))#(4)
-        self.assertIn('<title>To-Do lists</title>', html)#(5)
-        self.assertTrue(html.endswith('</html>'))#(4)
+        request = HttpRequest()  # (1)
+        response = home_page(request)  # (2)
+        html = response.content.decode('utf8')  # (3)
+        self.assertTrue(html.startswith('<html>'))  # (4)
+        self.assertIn('<title>To-Do lists</title>', html)  # (5)
+        self.assertTrue(html.endswith('</html>'))  # (4)
