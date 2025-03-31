7b6d1535b66afd7458aab61b42b9429bb3a57cf1
I am wuyanzu <12574809+lengpeihang--wuyanzu@user.noreply.gitee.com>
2025-03-31 10:58:16 +0800

    message 9:After refactoring the functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 张三听说有一个在线待办事项的应用
         # 他去看了这个应用的首页
@@ -23,21 +28,34 @@ class NewVisitorTest(unittest.TestCase):
 
         # 应用有一个输入待办事项的文本输入框
         inputbox = self.browser.find_element(By.ID, 'id_new_item')  # (1)
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # 他在文本输入框中输入了"Buy flowers"
+        inputbox.send_keys('Buy flowers')  # (2)
+        # 他按了回车键键后，页面更新了
+        # 待办事项表格中显示了“1: Buy flowers”
+        inputbox.send_keys(Keys.ENTER)  # (3)
+        time.sleep(1)  # (4)
+        self.check_for_row_in_list_table('1: Buy flowers')
+
+        # 页面中又显示了一个文本输入框，可以输入其他待办事项
+        # 他输入了“Give a gift to Lisi”
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')  # (1)
-        self.assertIn('1: Buy flowers', [row.text for row in rows])
-        self.assertIn('2:Give a gift to Lisi', [row.text for row in rows])
-        # 页面中又显示了一个文本输入框，可以输入其他待办事项
-        # 他输入了"gift to girlfriend"
+        # 页面再次更新，她的清单中显示了这两个待办事项
+        self.check_for_row_in_list_table('1: Buy flowers')
+        self.check_for_row_in_list_table('2: Give a gift to Lisi')
+
+        # 张三想知道这个网站是否会记住她的清单
+        # 他看到网站为他生成了一个唯一的URL
         self.fail('Finish the test!')
 
-        # 页面再次更新，她的清单中显示了这两个待办事项
-        # 张三想知道这个网站是否会记住他的清单
-        # 他看到网站为他生成了一个唯一的URL
         # 他访问那个URL，发现他的待办事项列表还在
         # 他满意的离开了
 
